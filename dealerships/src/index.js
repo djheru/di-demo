e7ba187263b8e4abb2eec812e5c87f8fed03822f
Philip Damra <pdamra@dealerinspire.com>
2021-03-19 00:48:57 -0700

    Update index.js and add a few styles to customize the Amplify UI components

--- dealerships/src/index.js
+++ dealerships/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
+import 'antd/dist/antd.css';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router />,
   document.getElementById('root')
 );
 
