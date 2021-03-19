c2d220f7b96e35df66903851eb00b88804f76ad3
Philip Damra <pdamra@dealerinspire.com>
2021-03-19 01:08:33 -0700

    Wire up the DealershipList in the Public page

--- dealerships/src/Public.js
+++ dealerships/src/Public.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
 import Container from './Container';
+import DealershipList from './DealershipList';
 
 const Public = () => {
+  const [isAdmin, setIsAdmin ] = useState(false);
+
+  useEffect(() => {
+    checkIsAdmin()
+  }, []);
+
+  const checkIsAdmin = async () => {
+    try {
+      const sessionData = await Auth.currentSession();
+      const { idToken: { payload = {} } = {} } = sessionData;
+      const hasAdminGroup =  (
+        payload['cognito:groups'] && 
+        Array.isArray(payload['cognito:groups']) && 
+        payload['cognito:groups'].includes('Admin')
+      ) ? true : false;
+      setIsAdmin(hasAdminGroup);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <Container>
-      <h1>Public Route</h1>
+      <h1>Dealerships</h1>
+      <DealershipList isAdmin={ isAdmin } />
     </Container>
   );
 }
